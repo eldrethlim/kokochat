c686a1e3cd9b46dc95684fb7be28888116ccb972
eldrethlim <eldrethlim@gmail.com>
2015-08-10 09:08:42 +0800

    added animations

---
+++ client/assets/js/index.js
@@ -0,0 +1,15 @@
+var runAnimations = function(e, remove, animation) {
+  $(e).removeClass(remove).addClass('animated ' + animation);
+
+  setTimeout( function() {
+    $(e).removeClass('animated ' + animation);
+  }, 1000);
+};
+
+$(document).ready(function() {
+
+  runAnimations($('#title'), 'hide', 'animated fadeInDown');
+  setTimeout(function() {
+    runAnimations($('#chat-button'), '', 'animated pulse')
+  }, 1000);
+
